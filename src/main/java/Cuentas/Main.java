1306685e19fb513b1d52f140b8b2062a3c96e1d4
raquel.pereira <raquel.pereira@rpereira-pc.cie.tecnologias.es>
2024-02-17 13:58:48 +0100

    Updated comentarios nuevos

--- src/main/java/Cuentas/Main.java
+++ src/main/java/Cuentas/Main.java
@@ -1,5 +1,12 @@
 package Cuentas;
+/**
+ * La clase Main es la clase principal que contiene el método main para probar la funcionalidad de la clase CCuenta.
+ */
 public class Main {
+    /**
+     * Método principal que se encarga de iniciar la aplicación y probar la funcionalidad de la clase CCuenta.
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este caso).
+     */
 
     public static void main(String[] args) {
         CCuenta cuenta1;
@@ -10,6 +17,12 @@ public class Main {
         System.out.println("El saldo actual es"+ saldoActual );
         operativa_cuenta (cuenta1,2300);
     }
+    /**
+     * Realiza operaciones sobre una cuenta bancaria, como retirar dinero e ingresar dinero.
+     * @param cuenta1 La cuenta bancaria sobre la cual realizar las operaciones.
+     * @param cantidad La cantidad de dinero a retirar de la cuenta y luego a ingresar en ella.
+     */
+    
     public static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
 
         try {
